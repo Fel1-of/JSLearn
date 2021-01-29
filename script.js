5b1aaa6594dad73ed8111514d3bf4258c03813b3
Fel1-of <fivehellday@gmail.com>
2021-01-29 11:26:41 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let money = 1000; //Доход за месяц
+
+let income = "Фриланс";
+
+let addExpenses = "Интернет, такси";
+
+let deposit = true;
+
+let mission = 1000000;
+
+let period = 12;
+
+let budgetDay = money/30;
+
+
+
+
+
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+console.log(addExpenses.length);
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель заработать ${mission} рублей`);
+console.log(budgetDay);
+console.log(addExpenses.toLocaleLowerCase().split());
