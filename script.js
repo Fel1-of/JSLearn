77cd73e8adeaea8925a691be716aa562e2db8096
Fel1-of <fivehellday@gmail.com>
2021-02-04 22:21:07 +0700

    finl

--- script.js
+++ script.js
@@ -1,40 +1,42 @@
 'use strict';
-function isNumber(num){
+
+function isNumber(num) {
     return !isNaN(parseFloat(num)) && isFinite(parseFloat(num));
 }
 
-function game(){
+function game() {
     let rand = Math.floor(Math.random() * 100) + 1;
     let ans = prompt("Угадай число от 1 до 100");
-    function answer(){
-        if(!isNumber(ans) && ans!==null ){
-            ans=prompt("Введи число!");
-        }
-        if(!isNumber(ans) && ans!==null){
+
+    function answer() {
+        if (!isNumber(ans) && ans !== null) {
+            ans = prompt("Введи число!");
             answer();
         }
         return Number(ans);
     }
     answer();
-    function MoreLess(){
-        if(ans===null){
+
+    function moreLess() {
+        if (ans === null) {
             alert("Игра окончена");
-        }
-        else if(ans>rand){
-            ans=prompt("Загаданное число меньше. Введите новое:");
+        } else if (ans > 100 || ans < 1) {
+            ans = prompt("Введите число от 1 до 100!");
             answer();
-            MoreLess();
-        }
-        else if(ans<rand){
-            ans=prompt("Загаданное число больше. Введите новое:");
+            moreLess();
+        } else if (ans > rand) {
+            ans = prompt("Загаданное число меньше. Введите новое:");
             answer();
-            MoreLess();
-        } 
-        else{
+            moreLess();
+        } else if (ans < rand) {
+            ans = prompt("Загаданное число больше. Введите новое:");
+            answer();
+            moreLess();
+        } else {
             alert('Поздравляю, Вы угадали!!!');
         }
     }
-    MoreLess();
+    moreLess();
 
 
 
