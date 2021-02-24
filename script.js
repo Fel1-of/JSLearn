2cb17cfcef5a8f65099387e925b4dc5ce25864ea
Fel1-of <fivehellday@gmail.com>
2021-02-24 20:36:48 +0700

    gst

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
   
 window.addEventListener('DOMContentLoaded', () =>{
+  
     const timerHours = document.querySelector('#timer-hours'),
           timerMinutes = document.querySelector('#timer-minutes'),
           timerSeconds = document.querySelector('#timer-seconds');
@@ -33,4 +34,116 @@ window.addEventListener('DOMContentLoaded', () =>{
       };
   
     countTimer('10 July 2021');
+
   });
+
+  const toggleMenu = () => {
+
+    const btnMenu = document.querySelector('.menu'),
+        menu = document.querySelector('menu'),
+        closeBtn = document.querySelector('.close-btn'),
+        menuItems = menu.querySelectorAll('ul>li'),
+        menuLinks = menu.querySelectorAll('li>a');
+
+    const handlerMenu = () => menu.classList.toggle('active-menu');
+
+    btnMenu.addEventListener('click', handlerMenu); 
+    closeBtn.addEventListener('click', handlerMenu);
+    menuItems.forEach( elem => elem.addEventListener('click', handlerMenu));
+    
+    //Scroll animate 
+    function scroll(block, dur) {
+        const endPoint = block.offsetTop;
+
+        let idAnimate;
+        let num = 0;
+
+        function animateScroll() {  
+            idAnimate = requestAnimationFrame(animateScroll);
+            
+            // scroll speed
+            function duration(count) {
+                if (num > (endPoint * 0.95) && count !== 1) {
+                    count = count / 2;
+                } else {
+                    if (endPoint > 4000) {
+                        count += 3;
+                    } else  if (endPoint > 3000) {
+                        count += 2;
+                    }
+                }
+                num = num + (count * 20);  
+            }
+
+            duration(dur);
+            document.documentElement.scrollTop = num;
+
+            if (num > endPoint) {
+                document.documentElement.scrollTop = endPoint;
+                cancelAnimationFrame(idAnimate);
+            }
+        }
+
+        idAnimate = requestAnimationFrame(animateScroll);
+    }
+
+    menuLinks.forEach( item => {
+        const itemId = item.getAttribute('href').substring(1);
+        item.addEventListener('click', e => {
+            e.preventDefault();
+            const block = document.getElementById(itemId);
+            scroll(block, 3);
+        });
+    });
+
+    //Button to next slide at main block
+    let mainLinkImg = document.querySelector('main>a[href="#service-block"]');
+    mainLinkImg.addEventListener('click', (e) => {
+        e.preventDefault();
+        scroll(document.getElementById('service-block'), 1);
+    });
+
+};
+
+toggleMenu();
+
+
+{//Popup
+const togglePopUp = () => {
+    const popup = document.querySelector('.popup'),
+         popupBtn = document.querySelectorAll('.popup-btn'),
+         popupClose = document.querySelector('.popup-close'),
+         popupContent = popup.querySelector('.popup-content'),
+         popupContentRect =  popupContent.getBoundingClientRect(),
+         popupContentX = popupContentRect.x;
+    
+    popupBtn.forEach( elem => elem.addEventListener('click', () => {
+        popup.style.display = 'block';   
+        if (screen.width > 768) {
+            animationPopUp();
+        }
+    }));
+
+    popupClose.addEventListener('click', () => popup.style.display = 'none');
+
+    //Popup Animation
+    function animationPopUp() {
+        let animationId;
+        let count = -1200;
+        popupContent.style.transform = `translate(${count}px)`; 
+        
+            const animationFunc = () => {
+                animationId = requestAnimationFrame(animationFunc);
+                count += 50;
+                    if ( count >= popupContentX - 50 ) {
+                        cancelAnimationFrame(animationId);
+                    } 
+                    
+                popupContent.style.transform = `translate(${count}px)`;
+            };  
+        animationFunc();
+    }   
+};
+
+togglePopUp();
+}
