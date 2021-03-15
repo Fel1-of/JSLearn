7344792fec552a9a1f2325903498d6fcb4587d5f
Fel1-of <fivehellday@gmail.com>
2021-03-15 19:51:35 +0700

    les28-h

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 'use strict';
-function generateOddNumber(x, y){
-    let rand = Math.floor(Math.min(x, y) + Math.random() * (Math.max(x, y) + 1 - Math.min(x, y)));
-    console.log(rand);
-}
+const task1 = document.getElementById('task1');
+const task2 = document.getElementById('task2');
+const body = document.querySelector('body');
+// 1
+task1.innerHTML = task1.innerHTML.replace(/(функци(и|я))/ig, '<strong>$1</strong>');
 
-generateOddNumber(1, 100);
-generateOddNumber(0, -10);
-generateOddNumber(-7, -3);
-generateOddNumber(-100, 100);
-generateOddNumber(1, -1);
+// 2
+task2.innerHTML = task2.innerHTML.replace(/(\d+:\d+)/ig, `<b>$1</b>`);
+
+// // 3 
+task1.innerHTML = task1.innerHTML.replace(/(«[а-я]+[а-я\s]*(— )*[а-я\s]*[а-я]*»)/ig, '<mark>$1</mark>');
+task2.innerHTML = task2.innerHTML.replace(/("[а-я]+[а-я\s]*(— )*[а-я\s]*[а-я]*")/ig, '<mark>$1</mark>');
+
+
+// 4
+task2.innerHTML = task2.innerHTML.replace(/(http:\/\/)([^w]\w*\.\w{2,3})/ig, `<a href="$1$2">$2</a>`);
+
+// 5
+console.log(body.innerHTML.match(/#[a-f0-9]*/ig));
+
+// 6
+
+task2.innerHTML = task2.innerHTML.replace(/(http:\/\/)(\w*\.\w*.\w{2})(.*\/)/ig, `<a href="$1$2$3">$2</a>`);
